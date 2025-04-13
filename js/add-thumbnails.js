5d980bfecf93d8cd00ec7bda27b4966cea6d7498
Andrew Zakharchenko <drewtsure@gmail.com>
2025-04-13 22:30:35 +0300

    связывает модули и добавляет событие открытия увеличенного изображения

--- js/add-thumbnails.js
+++ js/add-thumbnails.js
@@ -1,17 +1,25 @@
 import { createThumbnails } from './create-thumbnails';
+import { openFullSizeImage } from './open-full-size-image';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictures = document.querySelector('.pictures');
 const usersPicturesData = createThumbnails();
 const usersPicturesFragment = document.createDocumentFragment();
 
-usersPicturesData.forEach(({url, description, likes, comments}) => {
+usersPicturesData.forEach((pictureData) => {
+  const {url, description, likes, comments} = pictureData;
   const picture = pictureTemplate.cloneNode(true);
 
   picture.querySelector('.picture__img').setAttribute('src', url);
   picture.querySelector('.picture__img').setAttribute('alt', description);
   picture.querySelector('.picture__likes').textContent = likes;
   picture.querySelector('.picture__comments').textContent = comments.length;
+
+  picture.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openFullSizeImage(pictureData);
+  });
+
   usersPicturesFragment.append(picture);
 });
 
