5182fbccf73ae0d9bc80aa237f7bdcd2d93ad6d0
Andrew Zakharchenko <drewtsure@gmail.com>
2025-04-25 17:32:55 +0300

    добавляет управление css фильтрами

--- js/upload-form.js
+++ js/upload-form.js
@@ -7,8 +7,12 @@ const uploadFormCloseButton = uploadForm.querySelector('.img-upload__cancel');
 const uploadFormSubmitButton = uploadForm.querySelector('.img-upload__submit');
 const uploadFormScaleSmaller = uploadForm.querySelector('.scale__control--smaller');
 const uploadFormScaleBigger = uploadForm.querySelector('.scale__control--bigger');
+const uploadFormScaleWrapper = uploadForm.querySelector('.img-upload__effect-level');
 const uploadFormScaleValue = uploadForm.querySelector('.scale__control--value');
-const uploadFormImagePreview = uploadForm.querySelector('.img-upload__preview');
+const uploadFormImagePreview = uploadForm.querySelector('.img-upload__preview img');
+const uploadFormEffectLevel = uploadForm.querySelector('.effect-level__slider');
+const uploadFormEffectLevelInput = uploadForm.querySelector('.effect-level__value');
+const uploadFormEffectsList = uploadForm.querySelector('.effects__list');
 
 const MAX_QUANTITY_OF_HASHTAGS = 5;
 const DESCRIPTION_MAX_LENGTH = 140;
@@ -56,6 +60,7 @@ function resetUploadFormFields () {
 
 function openUploadForm () {
   toggleSubmitButton();
+  uploadFormScaleWrapper.classList.add('hidden');
 
   uploadFormOverlay.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
@@ -100,7 +105,6 @@ const validateHashtags = (string) => {
     }
   }
   return result;
-  // #dfsdf #ererwe #ertre d # #ererwe # r
 };
 
 const getErrorsMessage = (allErrorsList, foundErrorsList) => {
@@ -144,17 +148,12 @@ uploadForm.addEventListener('submit', (evt) => {
 });
 
 
-// uploadFormScaleSmaller
-// uploadFormScaleBigger
-// uploadFormScaleValue
-// uploadFormImagePreview
-
 const SCALE_STEP = 25;
 const SCALE_MIN = 25;
 const SCALE_MAX = 100;
 
 let uploadFormImageScale = 100;
-uploadFormImagePreview.style.transform = 'scale(1)';
+uploadFormImagePreview.style.transform = `scale(${uploadFormImageScale / 100})`;
 
 function changeUploadImageScale (currentScale, operation) {
   if (operation === 'minus' && currentScale > SCALE_MIN) {
@@ -178,3 +177,111 @@ uploadFormScaleBigger.addEventListener('click', () => {
   uploadFormScaleValue.value = `${uploadFormImageScale}%`;
 });
 
+let currentEffect = 'original';
+
+const filterOptions = {
+  original: {
+    sliderOptions: {
+      range: { min: 0, max: 0 },
+      start: 0,
+      step: 1
+    },
+    filterForCSS: 'none',
+    filterPoints: ''
+  },
+  chrome: {
+    sliderOptions: {
+      range: { min: 0, max: 1 },
+      start: 1,
+      step: 0.1
+    },
+    filterForCSS: 'grayscale',
+    filterPoints: ''
+  },
+  sepia: {
+    sliderOptions: {
+      range: { min: 0, max: 1 },
+      start: 1,
+      step: 0.1
+    },
+    filterForCSS: 'sepia',
+    filterPoints: ''
+  },
+  marvin: {
+    sliderOptions: {
+      range: { min: 0, max: 100 },
+      start: 100,
+      step: 1
+    },
+    filterForCSS: 'invert',
+    filterPoints: '%'
+  },
+  phobos: {
+    sliderOptions: {
+      range: { min: 0, max: 3 },
+      start: 3,
+      step: 0.1
+    },
+    filterForCSS: 'blur',
+    filterPoints: 'px'
+  },
+  heat: {
+    sliderOptions: {
+      range: { min: 0, max: 3 },
+      start: 3,
+      step: 0.1
+    },
+    filterForCSS: 'brightness',
+    filterPoints: ''
+  }
+};
+
+noUiSlider.create(uploadFormEffectLevel, {
+  range: {
+    min: 0,
+    max: 0,
+  },
+  start: 0,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  }
+});
+
+function updateFilterLevel () {
+  const cssFilter = filterOptions[currentEffect].filterForCSS;
+  const cssAmount = uploadFormEffectLevel.noUiSlider.get();
+  const cssPoints = filterOptions[currentEffect].filterPoints;
+
+  uploadFormImagePreview.style.filter = `${cssFilter}(${cssAmount}${cssPoints})`;
+  uploadFormEffectLevelInput.value = cssAmount;
+}
+
+uploadFormEffectLevel.noUiSlider.on('update', () => {
+  updateFilterLevel();
+});
+
+uploadFormEffectsList.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('effects__radio')) {
+    currentEffect = evt.target.value;
+
+    if (currentEffect === 'none') {
+      currentEffect = 'original';
+      uploadFormImagePreview.style.removeProperty('filter');
+      uploadFormScaleWrapper.classList.add('hidden');
+    } else {
+      uploadFormScaleWrapper.classList.remove('hidden');
+    }
+
+    const options = filterOptions[currentEffect].sliderOptions;
+    uploadFormEffectLevel.noUiSlider.updateOptions(options);
+  }
+});
