775e31241664e339563e0d4891af6a4890e58067
Andrew Zakharchenko <drewtsure@gmail.com>
2025-05-05 12:06:21 +0300

    чистит main от комментария

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// Если при загрузке данных с сервера произошла ошибка запроса, нужно показать соответствующее сообщение. Разметку сообщения, которая находится в блоке #data-error внутри шаблона template, нужно разместить перед закрывающим тегом </body>. Сообщение удаляется со страницы через 5 секунд.
-
 import { getData } from './api.js';
 import { renderThumbnails, showImageDownloadError } from './add-thumbnails.js';
 import './open-full-size-image.js';
